9bf4c29c459765c11565520513b03c1b151236de
Stan <drittachita@gmail.com>
2019-02-18 14:31:30 -0500

    coding the guest list app in vue

--- GuestList-App-Starter/app.js
+++ GuestList-App-Starter/app.js
@@ -2,7 +2,6 @@ new Vue({
     el: '#app',
     data: {
         event: {
-            eventHeading: 'Guest List',
             eventDate: 'August 14th - 16th',
             eventTitle: 'Summer Festival!',
             eventTitle2: 'v text output',
@@ -10,19 +9,49 @@ new Vue({
             eventDesc: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
         }, 
         newNameText: '',
-        guestName: ['James','Chris','Sam'],
-        formSubmitClass: "",
-        appStyles: {
-            marginTop: '25px'
-        }
+        limitText: '',
+        guestName: [],
+        eventCapacity: 25,
+        eventCapacityPercent: 0
     },
     methods:{
         formSubmitted: function() {
-            if(this.newNameText.length > 0){
+            if(this.newNameText.length > 0 && this.eventCapacityPercent < 100){
                 this.guestName.push(this.newNameText)
                 this.newNameText = ''
-                this.formSubmitClass = "submitted"
+                this.eventCapacityPercent = this.guestName.length / (this.eventCapacity / 100)
+            } 
+            if (this.eventCapacityPercent == 100){
+                this.limitText = "You've reached the guest limit"
             }
+        },
+        keyPressed: function() {
+            console.log("@keyup.32='keyPressed'")
         }
+    },
+    computed: {
+        sortNames: function() {
+            return this.guestName.sort()
+        }
+    },
+    watch: {
+        guestName: function(data) {
+            console.log('watch triggered')
+        }
+    },
+    filters: {
+        toUpper: function (value) {
+            return value.toUpperCase()
+        },
+        formatName: function (value){
+            return value.slice(0, 1).toUpperCase() + value.slice(1).toLowerCase()
+        }
+    }
+});
+
+new Vue ({
+    el: '#navigation',
+    data: {
+        appName: 'Guest List'
     }
 });
