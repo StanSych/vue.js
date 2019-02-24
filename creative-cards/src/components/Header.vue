8a6f7b95f28915371c86a16dff91fac7e261c921
Stan <drittachita@gmail.com>
2019-02-24 15:50:05 -0500

    creative cards app: adding local and global components

---
+++ creative-cards/src/components/Header.vue
@@ -0,0 +1,28 @@
+//STRUCTURE
+<template>
+    <div>
+        <h1 class="text-center">Creative Cards</h1>
+        <ul class="nav justify-content-center">
+            <li class="nav-item"><a href="" class="nav-link">Front</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Inside Left</a></li> 
+            <li class="nav-item"><a href="" class="nav-link">Inside Right</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Back</a></li>
+        </ul>
+    </div>
+</template>
+
+//JS
+<script>
+
+</script>
+
+//STYLES
+<style lang="scss">
+    h1{
+        margin-top: 1rem;
+    }
+
+    a{
+        cursor: pointer;
+    }
+</style>
