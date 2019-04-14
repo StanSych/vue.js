16147f74ba0ac9dd51218999a56a12396d324386
Stan <drittachita@gmail.com>
2019-04-14 17:20:51 -0400

    creative-cards: styling the text output

--- creative-cards/src/components/card/TextOutput.vue
+++ creative-cards/src/components/card/TextOutput.vue
@@ -1,7 +1,52 @@
 //STRUCTURE
 <template>
-    <div>
-        <p :style="styleObject">
+    <div @mouseover="showOptions = true" @mouseleave="showOptions = false">
+        <form class="small" v-show="showOptions">
+            <label for="selectBox">Font Size:</label>
+            <select class="custom-select" id="selectBox" v-model="setFontSize">
+                <option value="42">42px</option>
+                <option value="48">48px</option>
+                <option value="56">56px</option>
+                <option value="64">64px</option>
+            </select>
+
+            <div class="form-check form-check-inline">
+                <label class="form-check-label" for="">
+                    <input type="radio" class="form-check-input" value="left" v-model="setTextAlign">
+                    Left
+                </label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <label class="form-check-label" for="">
+                    <input type="radio" class="form-check-input" value="center" v-model="setTextAlign">
+                    Center
+                </label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <label class="form-check-label" for="">
+                    <input type="radio" class="form-check-input" value="right" v-model="setTextAlign">
+                    Right
+                </label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <label for="" class="form-check-input">
+                    <input type="checkbox" class="form-check-input" v-model="setBold">
+                    <strong>Bold</strong>
+                </label>
+            </div>
+
+             <div class="form-check form-check-inline">
+                <label for="" class="form-check-input">
+                    <input type="checkbox" class="form-check-input" v-model="setItalic">
+                    <i>Italic</i> 
+                </label>
+            </div>
+        </form>
+
+        <p :style="styleObject" :class="{ bold: setBold, italic: setItalic }">
             {{ displayText }}
         </p>
     </div>
@@ -17,10 +62,21 @@
                default: 200
            }
        },
+       data: function() {
+           return {
+               showOptions: false,
+               setFontSize: '',
+               setTextAlign: '',
+               setBold: false,
+               setItalic: false
+           }
+       },
        computed: {
            styleObject: function(){
                return {
-                   height: this.containerHeight + 'px'
+                   height: this.containerHeight + 'px',
+                   fontSize: this.setFontSize + 'px',
+                   textAlign: this.setTextAlign
                }
            }
        }
@@ -29,7 +85,6 @@
 
 //STYLES
 <style lang="scss" scoped>
-    //font-family: 'Playfair Display', serif;
    p{
        font-family: 'Playfair Display', serif;
        font-size: 42px;
@@ -41,4 +96,26 @@
        white-space: pre-line;
        overflow: hidden;
    }
+
+   .bold {
+       font-weight: bold;
+   }
+
+
+    .italic {
+        font-style: italic;
+    }
+
+    form {
+        position: absolute;
+        border-bottom: 1px dotter grey;
+        margin-top: 10px;
+        margin-bottom: 5px;
+        padding-bottom: 5px;
+    }
+
+    select {
+        height: 40%;
+    }
+
 </style>
