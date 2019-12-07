1cbf26dd350e8b068ef288ceba8d6498f49bf80c
Stan <drittachita@gmail.com>
2019-12-07 12:48:48 -0500

    real-word-vue: course material code (components)

---
+++ real-world-vue/src/views/EventList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Events Listings</h1>
+        <EventCard/>
+    </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+
+export default {
+    components: {
+        EventCard,
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
