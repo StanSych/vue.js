1278b51e28c46ce875a7e9ee0c94705a2861db90
Stan <drittachita@gmail.com>
2020-02-15 14:40:27 -0500

    Building Nuxt.js app

---
+++ real-world-nuxt/pages/create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event Listing',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Page where you can crate an event for slavs. Our slogan: syka blyat for fun'
+        }
+      ]
+    }
+  }
+}
+</script>
